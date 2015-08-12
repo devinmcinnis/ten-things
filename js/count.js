de201d9442eaca902851f60c5397a5f306a9bc97
Devin McInnis <devin.mcinnis@gmail.com>
2015-08-11 21:37:45 -0700

    init commit for app; working FE, no BE

--- js/count.js
+++ js/count.js
@@ -9,26 +9,44 @@
 
   var $document = $(document);
 
+  function buildNewLine(id) {
+    return '<li data-id="' + id + '">' +
+              '<label>' +
+                '<span class="c-thing__label">#' + id + '</span>' +
+                '<input name="thing' + id + '" class="c-thing__input" type="text" placeholder="The next thing is next">' +
+              '</label>' +
+            '</li>';
+  }
+
   $document.ready(function() {
-    $document.on('click', 'button', function() {
-      var $button = $(this),
-          $count = $('#count');
+    $document.on('submit', 'form#tenthings', function(e) {
+      e.preventDefault();
+      $(this).remove();
+      $('main').append('Thanks for playing. Come back tomorrow!');
+    });
 
-      $.post('/count/increment', function(res) {
-        if (res.newCount >= 4) {
-          $count.text(42);
-          $button.remove();
+    $document.on('keypress', 'input', function(e) {
+      var $input = $(this);
 
-          var img = $('<img>', {
-            src: 'img/goat.jpg'
-          });
+      // 13 is [enter]
+      if (e.keyCode === 13) {
+        // Prevent form from submitting
+        e.preventDefault();
 
-          $('body').append(img);
-        } else {
-          $count.text(res.newCount);
+        if ($input.val().length === 0) {
+          return false;
         }
 
-      });
+        var $list = $('ul'),
+        id = parseInt($input.closest('li').data('id'), 10) + 1;
+
+        var $newListItem = $(buildNewLine(id));
+        $list.append($newListItem);
+
+        setTimeout(function() {
+          $('input[name="thing'+id+'"]').focus();
+        }, 0);
+      }
     });
   });
 })();
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,9 @@ var Routes = function(app) {
   app.get('/', function(req, res) {
     res.render('index.jade', {
       title: Config.title + 'Home',
-      count: Count.get()
+      count: Count.get(),
+      something: process.env.THING,
+      thing: process.env.THING || "Hmm, there doesn't seem to be anything to talk about today. Check back tomorrow!"
     });
   });
 

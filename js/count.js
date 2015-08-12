1ed0a006374b2b302c04fbd5d67ba9ea929d9169
Devin McInnis <devin.mcinnis@gmail.com>
2015-08-12 09:24:05 -0700

    temp store things on server side

--- js/count.js
+++ js/count.js
@@ -10,7 +10,7 @@
   var $document = $(document);
 
   function buildNewLine(id) {
-    return '<li data-id="' + id + '">' +
+    return  '<li data-id="' + id + '">' +
               '<label>' +
                 '<span class="c-thing__label">#' + id + '</span>' +
                 '<input name="thing' + id + '" class="c-thing__input" type="text" '+
@@ -19,14 +19,36 @@
             '</li>';
   }
 
+  function buildListOfThings(thing) {
+    return  '<li>' +
+              '<p>' + thing + '</p>' +
+            '</li>';
+  }
+
   $document.ready(function() {
     $document.on('submit', 'form#tenthings', function(e) {
       e.preventDefault();
-      $(this).remove();
-      $('main').append('Thanks for playing. Come back tomorrow!');
+      var $form = $(this),
+        data = $form.serializeArray();
+
+      $.post('/things', data, function(res) {
+        console.log(res);
+
+        $form.remove();
+
+        var listOfThings = '<ul>';
+
+        for (var i = 0; i < res.things.length; i += 1) {
+          listOfThings += buildListOfThings(res.things[i]);
+        }
+
+        listOfThings += '</ul>';
+
+        $('main').append(listOfThings);
+      });
     });
 
-    $document.on('keypress', 'input', function(e) {
+    $document.on('keypress', '.c-thing__input', function(e) {
       var $input = $(this);
 
       // 13 is [enter]
--- src/routes/count.js
+++ src/routes/count.js
@@ -3,7 +3,24 @@
 var Config = require('../config/config.js')();
 var Count = require('../models/count.js');
 
+var things = [];
+
 module.exports = function(app) {
+  app.post('/things', function(req, res) {
+    console.log(req.body);
+
+    for (var thing in req.body) {
+      if (req.body[thing]) {
+        things.push(req.body[thing]);
+      }
+    }
+    console.log('things', things);
+
+    res.json({
+      things: things
+    });
+  });
+
   app.post('/count/increment', function(req, res) {
     res.json({
       newCount: Count.increment()
